4d9fe438c5eb974b6306badb023e3d15571e682f
TUTUU9 <tarasuzun@gmail.com>
2026-04-14 19:41:28 +0200

    feat: add user login and jwt authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
 
 const register = async (req, res) => {
     try {
@@ -41,7 +42,52 @@ const register = async (req, res) => {
         });
     }
 };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body || {};
+        
+        if (!email || !password) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Email and password are required',
+            });
+        }
+        const user = await User.findOne({ email });
 
+        if (!user) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid email or password'
+            });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid email or password'
+            });
+        }
+
+        const token = generateToken(user);
+
+        user.password = undefined;
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'Successfully logged in',
+            token,
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Server error'
+        });
+    }
+};
 module.exports = {
-    register,
+    register, login
 };
