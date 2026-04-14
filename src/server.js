a4dff4125a98cb776a1a8f9f320bf8c90f333ba9
TUTUU9 <tarasuzun@gmail.com>
2026-04-14 19:04:53 +0200

    feat: connect mongodb with mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app');
+require('dotenv').config();
+const connectDB = require('./config/db');
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
