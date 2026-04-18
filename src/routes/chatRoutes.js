5b3ccfa9d8946fe92eff5bdca76d1a40a5fd3260
TUTUU9 <tarasuzun@gmail.com>
2026-04-18 19:09:29 +0200

    feat: add request human flow and improve chat status logic

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createChat , updateAISettings , assignedToSupport , changeChatStatus } = require('../controllers/chatController');
+const { createChat , updateAISettings , assignedToSupport , changeChatStatus , requestHuman} = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -8,5 +8,5 @@ router.post('/', authMiddleware, createChat);
 router.patch('/:chatId/ai-settings', authMiddleware, updateAISettings);
 router.patch('/:chatId/assign-support', authMiddleware, assignedToSupport);
 router.patch('/:chatId/status', authMiddleware, changeChatStatus);
-
+router.patch('/:chatId/request-human', authMiddleware, requestHuman);
 module.exports = router;
